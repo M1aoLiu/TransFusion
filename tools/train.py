1b4fb2f7e8d3bc043e99243e0eccf5382c7e5aac
M1aoLiu <2518747873@qq.com>
2024-03-21 21:43:05 +0800

    111

--- tools/train.py
+++ tools/train.py
@@ -241,6 +241,7 @@ def main():
     # add an attribute for visualization convenience
     model.CLASSES = datasets[0].CLASSES
     print("The distribution of this dataset is:\n{}".format(datasets))
+    print("The distribution of this dataset is:\n{}".format(datasets))
     train_detector(
         model,
         datasets,
